8b92b10da0f0e6627307065ffd98b57292386766
djalmocruzjr <djalmo.cruz@gmail.com>
2015-02-03 21:51:06 -0200

    [INFO] Atualizando os arquivos do repositorio.

--- database/curso-crud-java-jdbc-db.sql
+++ database/curso-crud-java-jdbc-db.sql
@@ -1,13 +1,13 @@
-DROP SCHEMA IF EXISTS `tutorial_crud_java_jdbc_db` ;
-CREATE SCHEMA IF NOT EXISTS `tutorial_crud_java_jdbc_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `tutorial_crud_java_jdbc_db` ;
+DROP SCHEMA IF EXISTS `curso_crud_java_jdbc_db` ;
+CREATE SCHEMA IF NOT EXISTS `curso_crud_java_jdbc_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `curso_crud_java_jdbc_db` ;
 
 -- -----------------------------------------------------
--- Table `tb_categories`
+-- Table `curso_crud_java_jdbc_db`.`tb_categories`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tb_categories` ;
+DROP TABLE IF EXISTS `curso_crud_java_jdbc_db`.`tb_categories` ;
 
-CREATE TABLE IF NOT EXISTS `tb_categories` (
+CREATE TABLE IF NOT EXISTS `curso_crud_java_jdbc_db`.`tb_categories` (
   `category_id` INT NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`category_id`))
@@ -15,11 +15,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `tb_products`
+-- Table `curso_crud_java_jdbc_db`.`tb_products`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tb_products` ;
+DROP TABLE IF EXISTS `curso_crud_java_jdbc_db`.`tb_products` ;
 
-CREATE TABLE IF NOT EXISTS `tb_products` (
+CREATE TABLE IF NOT EXISTS `curso_crud_java_jdbc_db`.`tb_products` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
   `product_name` VARCHAR(255) NOT NULL,
   `product_description` VARCHAR(255) NULL,
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `tb_products` (
   INDEX `fk_tb_products_tb_categories_idx` (`product_category_id` ASC),
   CONSTRAINT `fk_tb_products_tb_categories`
     FOREIGN KEY (`product_category_id`)
-    REFERENCES `tb_categories` (`category_id`)
+    REFERENCES `curso_crud_java_jdbc_db`.`tb_categories` (`category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
